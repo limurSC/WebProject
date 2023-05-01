0bbb6816973fcda23c7e20ff0a3302de204737a6
PotemkinS <81625479+PotemkinS@users.noreply.github.com>
2023-05-01 12:53:48 +0500

    ver 0.0000001

---
+++ Сайт/work/Controllers/RegistrationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using work.Models;
+
+namespace work.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Registration regInfo)
+        {
+            if(ModelState.IsValid)
+                return Redirect("/");
+
+            return View("Index");
+        }
+    }
+}
---
+++ Сайт/work/Models/Registration.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace work.Models
+{
+    public class Registration
+    {
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage ="Введите имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Введите Фамилию")]
+        public string Surname { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Введите почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Пароль")]
+        [Required(ErrorMessage = "Введите пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Подтверждение пароля")]
+        [Required(ErrorMessage = "Подтвердите пароль")]
+        [MinLength(8, ErrorMessage ="Пароль не менее 8 символов")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
